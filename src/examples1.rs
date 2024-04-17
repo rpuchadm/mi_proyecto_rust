17b2faa7278809e3573e887e12fa5560c6bfa70b
Rafael Puchades <rpuchadm@cfp.upv.es>
2024-04-17 23:22:48 +0200

    type

--- src/examples1.rs
+++ src/examples1.rs
@@ -6,7 +6,8 @@ pub fn examples1() {
 
     let mut arr = fun_decl();
     
-    fun_maxmin(arr);
+    let (max,min) = fun_maxmin(arr);
+    println!("El mayor es {} y el menor es {}", max, min);
 
     let sum = fun_sum(arr);
     println!("La suma es {}", sum);
@@ -19,7 +20,9 @@ pub fn examples1() {
     fun_sortdesc(arr);
 }
 
-fn fun_sortdesc(mut arr: [i32; 5]) {
+type TypeArr5i = [i32; 5];
+
+fn fun_sortdesc(mut arr: TypeArr5i) {
     println!("\n\nmuestra los enteros del array ordenados de mayor a menor");
     arr.sort_by(|a, b| b.cmp(a));
     for i in arr.iter() {
@@ -27,7 +30,7 @@ fn fun_sortdesc(mut arr: [i32; 5]) {
     }
 }
 
-fn fun_sortasc(arr: &mut [i32; 5]) {
+fn fun_sortasc(arr: &mut TypeArr5i) {
     println!("\n\nmuestra los enteros del array ordenados de menor a mayor");
     arr.sort();
     for i in arr.iter() {
@@ -35,13 +38,13 @@ fn fun_sortasc(arr: &mut [i32; 5]) {
     }
 }
 
-fn fun_avg(sum: i32, arr: [i32; 5]) -> f64 {
+fn fun_avg(sum: i32, arr: TypeArr5i) -> f64 {
     println!("\n\nmuestra la media de los enteros del array");
     let avg = sum as f64 / arr.len() as f64;
     avg
 }
 
-fn fun_sum(arr: [i32; 5]) -> i32 {
+fn fun_sum(arr: TypeArr5i) -> i32 {
     println!("\n\nmuestra la suma de los enteros del array");
     let mut sum = 0;
     for i in arr.iter() {
@@ -50,7 +53,7 @@ fn fun_sum(arr: [i32; 5]) -> i32 {
     sum
 }
 
-fn fun_maxmin(arr: [i32; 5]) {
+fn fun_maxmin(arr: TypeArr5i) -> (i32, i32){
     println!("\n\nmuestra el mayor y el menor de los enteros del array");
     let mut max = arr[0];
     let mut min = arr[0];
@@ -62,10 +65,10 @@ fn fun_maxmin(arr: [i32; 5]) {
             min = arr[i];
         }
     }
-    println!("El mayor es {} y el menor es {}", max, min);
+    (max, min)
 }
 
-fn fun_decl() -> [i32; 5] {
+fn fun_decl() -> TypeArr5i {
     let mut rng = rand::thread_rng();
     println!("\n\ndeclara un array de 5 enteros aleatorios y muestra su contenido");
     let mut arr = [0; 5];
